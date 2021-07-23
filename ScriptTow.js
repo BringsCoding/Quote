2cdf2e850bf92f205e980e5d3fc513418927b60a
BringsCoding <85495653+BringsCoding@users.noreply.github.com>
2021-07-23 12:09:19 +0200

    Update ScriptTow.js

--- ScriptTow.js
+++ ScriptTow.js
@@ -1,7 +1,9 @@
 let button = document.querySelector("#button");
+let textfeld = document.querySelector("#text");
 let arr = [];
 let arrAuthor = [];
-let clear = [];
+let i = 0;
+
 button.addEventListener("click", function () {
   fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
     .then(function (response) {
@@ -10,22 +12,7 @@ button.addEventListener("click", function () {
     .then(function (data) {
       let Text = data.data[0].quoteText;
       let Author = data.data[0].quoteAuthor;
-      arr.push(Text);
-      arrAuthor.push(Author);
+      document.getElementById("text-top").innerHTML = Text;
+      document.getElementById("text-down").innerHTML = Author;
     });
-
-  document.getElementById("text-top").innerHTML = JSON.stringify(arr).slice(
-    1,
-    -1
-  );
-
-  document.getElementById("text-down").innerHTML = JSON.stringify(
-    arrAuthor
-  ).slice(1, -1);
 });
-
-//console.log(data.data[0].quoteText);
-//console.log(data.data[0].quoteAuthor);
-//.then(function (response) {
-//return response.json();
-//})
